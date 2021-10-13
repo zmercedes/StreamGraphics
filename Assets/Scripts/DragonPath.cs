4ff152ca67cbf6becbb67a7b4d57c321c52d844d
Zoilo <zmercedes89@gmail.com>
2021-10-13 18:47:06 -0400

    feat: DragonPath: add constants

--- Assets/Scripts/DragonPath.cs
+++ Assets/Scripts/DragonPath.cs
@@ -13,24 +13,35 @@ public class DragonPath : MonoBehaviour {
 	Vector2[] currentLine;
 	int total;
 	int current = 0;
+	int totalFrames;
+	int fadeOutFrames = 60;
+	float lineLength = 0.5f;
 
-	void Awake(){
+	void Awake() {
 		Application.targetFrameRate = 60;
 		line = GetComponent<Polyline>();
 		currentLine = new Vector2[path.Length + 1];
-		Color[] c = new Color[path.Length+1];
-		currentLine[0] = Vector2.down *0.5f;
+		Color[] c = new Color[path.Length + 1];
+		currentLine[0] = Vector2.down * 0.5f;
 		currentLine[1] = Vector2.zero;
 		c[0] = Color.black;
 		c[1] = Color.black;
 		point = Vector2.zero;
-		for (int i = 2; i< path.Length + 1; i++) {
+		for (int i = 2; i < path.Length + 1; i++) {
 			SetDirection(path[i - 2]);
-			point += direction * 0.5f;
+			point += direction * lineLength;
 			currentLine[i] = point;
 			c[i] = Color.black;
 		}
+		Vector3 pos = transform.position;
+		if (pos.x == 0) 
+			pos.y = (pos.y > 0) ? lineLength : lineLength * -1;
+		else 
+			pos.x = (pos.x > 0) ? lineLength : lineLength * -1;
+
+		transform.position = pos;
 		line.SetPoints(currentLine, c);
+		totalFrames = currentLine.Length + fadeOutFrames;
 		total = line.points.Count ;
 		int half = total / 2;
 		/*for(int i = 0; i < half; i++) {
@@ -44,9 +55,18 @@ public class DragonPath : MonoBehaviour {
 	}
 
 	void Update() {
+		current++;
 		if(current < currentLine.Length) {
 			line.SetPointColor(current, lineColor);
-			current++;
+		} else {
+			if (current < totalFrames) {
+				float t = (float)(current - currentLine.Length) / fadeOutFrames;
+				line.Color = Color.Lerp(Color.white, Color.black, t);
+			}
+			else {
+				ResetColor();
+				current = 0;
+			}
 		}
 		/*if(current < total) {
 			SetDirection(path[current]);
@@ -73,10 +93,7 @@ public class DragonPath : MonoBehaviour {
 
 			current = 0;
 		}*//*
-		if(current == total) {
-			SetFullColor();
-			current++;
-		}
+		
 		if(current > total) {
 
 		
@@ -85,10 +102,11 @@ public class DragonPath : MonoBehaviour {
 			for*/
 	}
 
-	void SetFullColor() { 
+	void ResetColor() { 
 		for(int i = 0; i < total; i++) {
-			line.SetPointColor(i, lineColor);
+			line.SetPointColor(i, Color.black);
 		}
+		line.Color = Color.white;
 	}
 
 
